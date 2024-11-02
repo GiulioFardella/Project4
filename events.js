ded083d3b592483c8de35a0f45c7123fcdc3f7a0
GiulioFardella <giulio.fardella@outlook.it>
2024-11-02 17:16:48 +0100

    adds mods

---
+++ events.js
@@ -0,0 +1,23 @@
+//// BOTTONE 
+window.addEventListener("scroll", function() {
+    const headerHeight = document.querySelector("header").offsetHeight;
+    const navbutton = document.getElementById("navbutton");
+  
+    if (window.scrollY > headerHeight) {
+      navbutton.classList.add("scrolled");
+    } else {
+      navbutton.classList.remove("scrolled");
+    }
+  });
+ //// NAVBAR 
+  window.addEventListener("scroll", function() {
+    const headerHeight = document.querySelector("header").offsetHeight;
+    const navBar = document.querySelector("nav");
+    
+    if (window.scrollY > headerHeight) {
+      navBar.classList.add("navbar-scrolled");
+    } else {
+      navBar.classList.remove("navbar-scrolled");
+    }
+  });
+  
